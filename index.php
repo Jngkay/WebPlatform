a4e89cb565dd1753d1f41f472aeaeb823accb589
Jngkay <angelicagrace.simbran@wvsu.edu.ph>
2022-06-22 17:43:40 +0800

    Add responsive illustration and text

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php include('include/header.php');?>
+<?php include('include/navbar.php');?>
+<link rel="stylesheet" href="css/index.css">
+<body>
+<div class="container">
+  <div class="row">
+    <div class="col-sm-6 left">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+      Laboriosam velit deserunt ea tempore et unde, adipisci neque consequatur. 
+    </div>
+    <div class="col-sm-6 right">
+    <img src="img/pic.jpg" class="img-fluid" alt="Responsive image">
+    </div>
+  </div>
+</div>
+    
+</body>
+</html>
+
